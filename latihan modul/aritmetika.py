95c52eeb64a2427a7f17214b4a6c5703602aded9
Zeto24 <109522475+Zeto24@users.noreply.github.com>
2024-03-16 06:08:59 +0700

    Add files via upload

---
+++ latihan modul/aritmetika.py
@@ -0,0 +1,20 @@
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if isinstance(a, int) and isinstance(b, int):
+        return a // b
+    if isinstance(a, float) or isinstance(b, float):
+        return a / b
+
+def sisaBagi(a, b):
+    return a % b
+
+def pangkat(a, b):
+    return a ** b
